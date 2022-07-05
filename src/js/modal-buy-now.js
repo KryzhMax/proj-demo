837e93338f95e2ab69d11dca70f39b16e719733e
Max Kryzhanovskyi <maximkryzhanovsky@gmail.com>
2022-07-05 23:04:23 +0300

    bugfix

--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,16 +1,16 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    openModalBtnMob: document.querySelector('[data-modal-open-mob]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     openModalBtnMob: document.querySelector('[data-modal-open-mob]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.openModalBtnMob.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtnMob.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('backdrop--hidden');
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle('backdrop--hidden');
+//   }
+// })();
